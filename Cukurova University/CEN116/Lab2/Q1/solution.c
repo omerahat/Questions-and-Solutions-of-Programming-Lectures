d1fbfa9d0341f73bb7895038cd40ed51c154e2ca
Berkantagur <berkantagur6@gmail.com>
2023-03-29 11:53:04 +0300

    .c

--- Cukurova University/CEN116/Lab2/Q1/solution.c
+++ Cukurova University/CEN116/Lab2/Q1/solution.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+void bubleSort(int *, int);
+void swap(int *, int *);
+
 int main(){
 
-    void bubleSort(int *, int);
-    void swap(int *, int *);
-
-    int array[] = {2,168,5,93,18,9,4,79,27,45};
+    int array[10] = {2,168,5,93,18,9,4,79,27,45};
+    int i;
+    printf("Array in original order:\n");
+    for (i=0; i<10; i++)
+        printf("%4d", array[i]);
 
 
     return 0;
