b633c02c45753d8dc9e8a945c6bdc28581d23b92
Berkantagur <berkantagur6@gmail.com>
2023-04-03 15:55:04 +0300

    .c

---
+++ Cukurova University/CEN116/Lab3/solution.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+void swap(int *a, int *b);
+int ascending(int a, int b);
+int descending(int a, int b); 
+void bubble(int a[], int size, int (*myFun)(int a, int b)); 
+
+int main(void) { 
+
+	int array[] = {6, 4, 8, 16, 10, 14, 12, 2, 18, 20}; 
+	int i, order; 
+
+	printf("Array in original order:\n"); 
+
+	for (i = 0; i < 10; i++) { 
+		printf("%4d", array[i]); 
+	} 
+
+	printf( "\nEnter 1 to sort in ascending order, 2 to sort in descending order:"); 
+    scanf("%d", &order); 
+    
+    if (order == 1) 
+        bubble(array, 10, ascending); 
+
+    else 
+        bubble(array, 10, descending); 
+
+    printf("Array in order:\n"); 
+
+    for (i = 0; i < 10; i++) { 
+        printf("%4d", array[i]); 
+    }
+
+    return 0; 
+} 
+
+void swap(int *a, int *b) { 
+    int temp; 
+    temp = *a; *a = *b; 
+    *b = temp; 
+} 
+
+int ascending(int a, int b) { 
+    return a > b; 
+} 
+
+int descending(int a, int b) { 
+    return a < b; 
+} 
+
+void bubble(int a[], int size, int (*myFun)(int a, int b)) { 
+    
+    int i, pass; 
+
+    for (pass = 0; pass < size - 1; pass++) { 
+
+        for (i = 0; i < size - 1; i++){
+
+            if ((*myFun)(a[i], a[i + 1])) 
+                swap(&a[i], &a[i + 1]);
+
+        }
+
+    } 
+
+}
